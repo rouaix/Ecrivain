d24733df31ed1c5b1c75f94d307709b53ed5a1f6
rouaix <daniel@rouaix.com>
2026-01-23 14:20:44 +0100

    Daniel dev

--- src/app/modules/project/controllers/ProjectController.php
+++ src/app/modules/project/controllers/ProjectController.php
@@ -1749,6 +1749,7 @@ class ProjectController extends Controller
 
         $title = $model->title ?: ($model->name ?? 'Sans titre');
         $content = $model->content ?? '';
+        $content = html_entity_decode($content, ENT_QUOTES | ENT_HTML5, 'UTF-8');
 
         header('Content-Type: application/json');
         echo json_encode([
